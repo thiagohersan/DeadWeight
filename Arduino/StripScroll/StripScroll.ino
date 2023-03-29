1892da1306cd259c199e3a202d852604cfb1b33a
Thiago Hersan <thiago.hersan+github@gmail.com>
2023-03-29 16:51:49 -0300

    adds 3rd phone tp StripScroll

--- Arduino/StripScroll/StripScroll.ino
+++ Arduino/StripScroll/StripScroll.ino
@@ -1,20 +1,25 @@
 #include <FastLED.h>
 #include "Phone.h"
 
-const int IPHONE_DATA_PIN = 2;
-Phone<14> iPhone;
+const int IPHONE_4_DATA_PIN = 2;
+Phone<14> iPhone4;
 
-const int SONY_DATA_PIN = 4;
-Phone<16> sonyPhone;
+const int IPHONE_G_DATA_PIN = 4;
+Phone<16> iPhoneG;
+
+const int HTC_OLD_DATA_PIN = 6;
+Phone<12> htcOld;
 
 void setup() {
   delay(10);
-  FastLED.addLeds<WS2812, IPHONE_DATA_PIN, GRB>(iPhone.leds, iPhone.getNumLeds());
-  FastLED.addLeds<WS2812, SONY_DATA_PIN, GRB>(sonyPhone.leds, sonyPhone.getNumLeds());
+  FastLED.addLeds<WS2812, IPHONE_4_DATA_PIN, GRB>(iPhone4.leds, iPhone4.getNumLeds());
+  FastLED.addLeds<WS2812, IPHONE_G_DATA_PIN, GRB>(iPhoneG.leds, iPhoneG.getNumLeds());
+  FastLED.addLeds<WS2812, HTC_OLD_DATA_PIN, GRB>(htcOld.leds, htcOld.getNumLeds());
 }
 
 void loop() {
-  iPhone.update();
-  sonyPhone.update();
+  iPhone4.update();
+  iPhoneG.update();
+  htcOld.update();
   FastLED.show();
 }
