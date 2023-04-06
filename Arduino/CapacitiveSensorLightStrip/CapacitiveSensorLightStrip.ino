79d6886d51ca9d0207c66d74f29e353e8523305e
Thiago Hersan <thiago.hersan+github@gmail.com>
2023-04-06 20:26:17 -0300

    clean up CapacitiveSensorLightStrip phone names/vars

--- Arduino/CapacitiveSensorLightStrip/CapacitiveSensorLightStrip.ino
+++ Arduino/CapacitiveSensorLightStrip/CapacitiveSensorLightStrip.ino
@@ -3,33 +3,26 @@
 
 const int SENSORS_PER_PHONE = 2;
 
-const int IPHONE_4_DATA_PIN = 2;
-const int IPHONE_4_LEDS_PER_SIDE = 14;
-Phone<IPHONE_4_LEDS_PER_SIDE, SENSORS_PER_PHONE> iPhone4({10, 11});
+const int HTC_G2_DATA_PIN = 2;
+const int HTC_G2_LEDS_PER_SIDE = 12;
+Phone<HTC_G2_LEDS_PER_SIDE, SENSORS_PER_PHONE> htcG2({10, 11});
 
 const int IPHONE_G_DATA_PIN = 4;
 const int IPHONE_G_LEDS_PER_SIDE = 16;
 // Phone<IPHONE_G_LEDS_PER_SIDE, SENSORS_PER_PHONE> iPhoneG({9, 8});
 
-const int HTC_OLD_DATA_PIN = 6;
-const int HTC_OLD_LEDS_PER_SIDE = 12;
-// Phone<HTC_OLD_LEDS_PER_SIDE, SENSORS_PER_PHONE> htcOld({11, 8});
-
 void setup() {
   Serial.begin(115200);
 
-  iPhone4.setup();
+  htcG2.setup();
   // iPhoneG.setup();
-  // htcOld.setup();
 
-  FastLED.addLeds<WS2812, IPHONE_4_DATA_PIN, GRB>(iPhone4.leds, iPhone4.getNumLeds());
+  FastLED.addLeds<WS2812, HTC_G2_DATA_PIN, GRB>(htcG2.leds, htcG2.getNumLeds());
   // FastLED.addLeds<WS2812, IPHONE_G_DATA_PIN, GRB>(iPhoneG.leds, iPhoneG.getNumLeds());
-  // FastLED.addLeds<WS2812, HTC_OLD_DATA_PIN, GRB>(htcOld.leds, htcOld.getNumLeds());
 }
 
 void loop() {
-  iPhone4.loop();
+  htcG2.loop();
   // iPhoneG.loop();
-  // htcOld.loop();
   FastLED.show();
 }
