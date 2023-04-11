44be5a8a28059644d371267504e376a5e87a8882
Thiago Hersan <thiago.hersan+github@gmail.com>
2023-04-11 18:21:07 -0300

    update phone names in LightStripScroll

--- Arduino/LightStripScroll/LightStripScroll.ino
+++ Arduino/LightStripScroll/LightStripScroll.ino
@@ -1,8 +1,8 @@
 #include <FastLED.h>
 #include "Phone.h"
 
-const int IPHONE_4_DATA_PIN = 2;
-Phone<14> iPhone4;
+const int HTC_G2_DATA_PIN = 2;
+Phone<14> htcG2;
 
 const int IPHONE_G_DATA_PIN = 4;
 Phone<16> iPhoneG;
@@ -12,13 +12,13 @@ Phone<12> htcOld;
 
 void setup() {
   delay(10);
-  FastLED.addLeds<WS2812, IPHONE_4_DATA_PIN, GRB>(iPhone4.leds, iPhone4.getNumLeds());
+  FastLED.addLeds<WS2812, HTC_G2_DATA_PIN, GRB>(htcG2.leds, htcG2.getNumLeds());
   FastLED.addLeds<WS2812, IPHONE_G_DATA_PIN, GRB>(iPhoneG.leds, iPhoneG.getNumLeds());
   FastLED.addLeds<WS2812, HTC_OLD_DATA_PIN, GRB>(htcOld.leds, htcOld.getNumLeds());
 }
 
 void loop() {
-  iPhone4.update();
+  htcG2.update();
   iPhoneG.update();
   htcOld.update();
   FastLED.show();
