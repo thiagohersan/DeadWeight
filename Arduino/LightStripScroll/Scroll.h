b1f4659c4e74ef4e2c1d4d5995bf4dcf3ade8ef3
Thiago Hersan <thiago.hersan+github@gmail.com>
2023-04-16 20:55:25 -0300

    adds pre-set parameters for scroll speeds in LightStripScroll

--- Arduino/LightStripScroll/Scroll.h
+++ Arduino/LightStripScroll/Scroll.h
@@ -2,18 +2,28 @@
 
 #include "Page.h"
 
+struct SpeedParams {
+  float V_INC_MULT;
+  float V_DAMP;
+};
+
 class Scroll {
+  private:
+    SpeedParams SLOW = { 0.6, 0.93 };
+    SpeedParams FAST = { 10.0, 0.87 };
+    SpeedParams mParams = SLOW;
+
   private:
     int PAGE_SIZE = sizeof(PAGE_VALUES);
     float A_DEC = 1.0;
     float A_INC_MIN = 12;
     float A_INC_MAX = 72;
     float V_MIN = 5.0;
-    float V_DAMP = 0.93;
+    float V_INC = mParams.V_INC_MULT * A_DEC;
+    float V_DAMP = mParams.V_DAMP;
 
   private:
     int y0 = 0;
-    float y_dir = -1.0;
     int height = 0;
     float v0 = 0;
     float a0 = 0;
@@ -28,7 +38,7 @@ class Scroll {
 
       if (a0 >= A_DEC) {
         a0 -= A_DEC;
-        v0 += A_DEC;
+        v0 += V_INC;
       } else {
         a0 = 0;
       }
@@ -37,18 +47,14 @@ class Scroll {
       if (v0 < random(V_MIN)) {
         // very slowed down
         if (a0 < A_DEC) {
-          // scrolling up with p() = 0.5 or scrolling down with p() = 0.125
-          if ((y_dir < 0 && !random(0, 2)) || (y_dir > 0 && !random(0, 8))) {
-            v0 = 0;
-            y_dir *= -1.0;
-          }
+          v0 = 0;
         }
         // bump a
         a0 += random(A_INC_MIN, A_INC_MAX);
       }
 
       v0 *= V_DAMP;
-      y0 = int(PAGE_SIZE + y0 + int(y_dir * v0)) % PAGE_SIZE;
+      y0 = int(y0 + v0) % PAGE_SIZE;
 
       lastUpdate = millis();
     }
